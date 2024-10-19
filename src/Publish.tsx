8d128cb60cbd7c519e553722ffb3a7cab10f9ddd
designernote <designernote@naver.com>
2024-10-20 06:03:54 +0900

    퍼블

--- src/Publish.tsx
+++ src/Publish.tsx
@@ -3,6 +3,15 @@ import { CartList } from "./publish/CartList";
 import { CartMain } from "./publish/CartMain";
 import { CartInfo } from "./publish/CartInfo";
 import { CartListDetail } from "./publish/CartListDetail";
+import { GiftRecommList } from "./publish/GiftRecommList";
+import { GiftRecommDetail } from "./publish/GiftRecommDetail";
+import { Mypage1 } from "./publish/Mypage1";
+import { Mypage2 } from "./publish/Mypage2";
+import { FriendList } from "./publish/FriendList";
+import { FriendInfo } from "./publish/FriendInfo";
+import { Question1 } from "./publish/Question1/Question1";
+import { Question2 } from "./publish/Question2/Question2";
+import { Question3 } from "./publish/Question3/Question3";
 
 function Publish() {
   return (
@@ -22,6 +31,33 @@ function Publish() {
             <li>
               <Link to="/publish/CartListDetail">CartListDetail</Link>
             </li>
+            <li>
+              <Link to="/publish/GiftRecommList">GiftRecommList</Link>
+            </li>
+            <li>
+              <Link to="/publish/GiftRecommDetail">GiftRecommDetail</Link>
+            </li>
+            <li>
+              <Link to="/publish/Mypage1">Mypage1</Link>
+            </li>
+            <li>
+              <Link to="/publish/Mypage2">Mypage2</Link>
+            </li>
+            <li>
+              <Link to="/publish/FriendList">FriendList</Link>
+            </li>
+            <li>
+              <Link to="/publish/FriendInfo">FriendInfo</Link>
+            </li>
+            <li>
+              <Link to="/publish/Question1">Question1</Link>
+            </li>
+            <li>
+              <Link to="/publish/Question2">Question2</Link>
+            </li>
+            <li>
+              <Link to="/publish/Question3">Question3</Link>
+            </li>
           </ul>
         </nav>
 
@@ -30,10 +66,26 @@ function Publish() {
             <Route path="/publish/CartList" component={CartList}></Route>
             <Route path="/publish/CartMain" component={CartMain}></Route>
             <Route path="/publish/CartInfo" component={CartInfo}></Route>
+
             <Route
               path="/publish/CartListDetail"
               component={CartListDetail}
             ></Route>
+            <Route
+              path="/publish/GiftRecommList"
+              component={GiftRecommList}
+            ></Route>
+            <Route
+              path="/publish/GiftRecommDetail"
+              component={GiftRecommDetail}
+            ></Route>
+            <Route path="/publish/Mypage1" component={Mypage1}></Route>
+            <Route path="/publish/Mypage2" component={Mypage2}></Route>
+            <Route path="/publish/FriendList" component={FriendList}></Route>
+            <Route path="/publish/FriendInfo" component={FriendInfo}></Route>
+            <Route path="/publish/Question1" component={Question1}></Route>
+            <Route path="/publish/Question2" component={Question2}></Route>
+            <Route path="/publish/Question3" component={Question3}></Route>
           </Switch>
         </div>
       </div>
--- src/publish/CartInfo/CartInfo.tsx
+++ src/publish/CartInfo/CartInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from "../Common/Header";
 import dummy4 from "./../../assets/images/dummy_4.png";
 import profile from "./../../assets/images/dummy_3.svg";
@@ -5,9 +6,19 @@ import giftBox from "./../../assets/images/giftBox.svg";
 import friend from "./../../assets/images/icon_friend.svg";
 
 export const CartInfo = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  // const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  // const openModal = () => setIsOpen(true);
+  const closeModal2 = () => setIsOpen2(false);
+
   return (
     <div className={"wrap"}>
-      <Header btn_back setting bell profile isBellActive={true} />
+      <Header btn_back bell profile isBellActive={true} />
       <div className="cardInfo">
         <div className="list_rounding">
           <div className="writing">
@@ -131,6 +142,55 @@ export const CartInfo = () => {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className="modal_new cartEdit">
+          <div className="dim"></div>
+
+          <div className="inner">
+            <div className="handler">
+              <button className="close" onClick={closeModal}></button>
+            </div>
+            <div className="input_area">
+              <small>바구니 이름</small>
+              <input type="text" placeholder="바구니 이름을 입력하세요" />
+              <small>기념일 수정</small>
+              <input type="text" placeholder="yyyy/mm/dd" />
+            </div>
+            <div className="edit_area">
+              바구니 이미지 수정<button className="btn_edit">edit</button>
+            </div>
+            <div className="public">
+              다른 사람에게 바구니 공개하기
+              <div className="toggle_area">
+                <input type="checkbox" id="toggle" hidden />
+
+                <label htmlFor="toggle" className="toggleSwitch">
+                  <span className="toggleButton"></span>
+                </label>
+              </div>
+            </div>
+            <button className="btn_public">바구니 정보 수정 완료</button>
+          </div>
+        </div>
+      )}
+      {isOpen2 && (
+        <div className="modal_new share">
+          <div className="dim"></div>
+
+          <div className="inner">
+            <div className="handler">
+              <button className="close" onClick={closeModal2}></button>
+            </div>
+            <div className="title">친한 친구를 바구니에 초대해보세요</div>
+            <div className="btn_share_area">
+              <button className="kakao">카카오톡</button>
+              <button className="link">링크복사</button>
+            </div>
+            <button className="btn_text">친구목록에서 초대하기</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
